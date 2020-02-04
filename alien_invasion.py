f7421d9dfa4e10dffbd3b90abfac5af0eb769463
Will Norton <willnorton@wills-mbp.home>
2020-02-03 21:09:32 -0600

    initial commit

---
+++ alien_invasion.py
@@ -0,0 +1,29 @@
+import sys 
+import pygame
+
+# ***Overall Class to manage game assets and behavior***
+class AlienInvasion:
+    
+    # Initialize the game and create game resources
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+
+    # Start the main loop of the game here
+    def run_game(self):
+        while True:
+            # Watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Make the most recently drawn screen visible
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # Make a game instance and run the game
+    ai = AlienInvasion()
+    ai.run_game()
