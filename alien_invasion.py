8ec2001a10e8de7522dce6b1954902755d024715
Will Norton <willnorton@wills-mbp.home>
2020-02-04 21:56:45 -0600

    update bullets method

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,14 +28,8 @@ class AlienInvasion:
             self._check_events()
             self._update_screen()
             self.ship.update()
-            self.bullets.update()
             self._update_screen()
-
-            # Get rid of bullets that have dissappeared
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-                #print(len(self.bullets))
+            self._update_bullets()
 
             # Make the most recently drawn screen visible
             # Runs faster when this is called here instead of _update_screen()
@@ -54,6 +48,7 @@ class AlienInvasion:
                 elif event.type == pygame.KEYUP:
                     self._check_keyup_events(event)
 
+
     # Responds to keypresses
     def _check_keydown_events(self, event):
          # Move the ship to the right
@@ -66,6 +61,7 @@ class AlienInvasion:
                     sys.exit()
         elif event.key == pygame.K_SPACE:
             self._fire_bullet()
+
     
     # Responds to key releases
     def _check_keyup_events(self, event):
@@ -75,10 +71,25 @@ class AlienInvasion:
         if event.key ==pygame.K_LEFT:
             self.ship.moving_left = False
 
+
     # Create a new bullet and add it to the bullets group
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+
+    # Update position of bullets and get rid of old bullets
+    def _update_bullets(self):
+        # Update bullet positions
+        self.bullets.update()
+        
+         # Get rid of bullets that have dissappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+            #print(len(self.bullets))
+
 
     # Update images on the screen and flip to new screen
     def _update_screen(self):
