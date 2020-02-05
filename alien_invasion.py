3d6dc759a174a737a10bf9cc756a72c966ece1bf
Will Norton <willnorton@wills-mbp.home>
2020-02-04 21:49:17 -0600

    Bullet Time

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 # ***Overall Class to manage game assets and behavior***
 class AlienInvasion:
@@ -18,6 +19,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
 
     # Start the main loop of the game here
@@ -26,9 +28,19 @@ class AlienInvasion:
             self._check_events()
             self._update_screen()
             self.ship.update()
+            self.bullets.update()
+            self._update_screen()
+
+            # Get rid of bullets that have dissappeared
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+                #print(len(self.bullets))
 
             # Make the most recently drawn screen visible
+            # Runs faster when this is called here instead of _update_screen()
             pygame.display.flip()
+            
 
     # Watch for keyboard and mouse events
     def _check_events(self):
@@ -52,6 +64,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
                     sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
     
     # Responds to key releases
     def _check_keyup_events(self, event):
@@ -61,12 +75,18 @@ class AlienInvasion:
         if event.key ==pygame.K_LEFT:
             self.ship.moving_left = False
 
+    # Create a new bullet and add it to the bullets group
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     # Update images on the screen and flip to new screen
     def _update_screen(self):
          # Redraw the screen during each pass of the loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
 
 if __name__ == '__main__':
